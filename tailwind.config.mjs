eb6ee136ea4b06c38485ed9df1980b7d64e690b7
Fikri <fikfan@gmail.com>
2024-03-20 14:28:20 +0800

    added custom fonts and colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    colors: {
+      black: {
+        primary: "#000000",
+        secondary: "#505050",
+        bg: "#D9D9D9", 
+      },
+      white: {
+        primary: "#FFFFFF",
+        bg: "#F3F3F3",
+      }
+    }
   },
   plugins: [],
 };
